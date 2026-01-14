78b1c5fa2f88b8963d0a39e2b2b5916a5a891310
Deimantė Burlėgaitė <vartotojas@Deimantes-MacBook-Pro.local>
2026-01-14 09:21:14 +0200

    Updated css and sort function

--- script.js
+++ script.js
@@ -4,31 +4,33 @@ const itemsWrapper = document.getElementById("items-wrapper");
 
 const items = await fetchAllItems();
 
-items.sort((a, b) => a.price - b.price);
+// items.sort((a, b) => a.price - b.price);
 
-items.forEach((i) => {
-  const card = document.createElement("a");
-  card.classList.add("card");
-  const link = `./item/index.html?id=${i.id}`;
-  card.href = link;
+[...items]
+  .sort((a, b) => a.price - b.price)
+  .forEach((i) => {
+    const card = document.createElement("a");
+    card.classList.add("card");
+    const link = `./item/index.html?id=${i.id}`;
+    card.href = link;
 
-  const image = document.createElement("img");
-  image.src = i.img_url;
+    const image = document.createElement("img");
+    image.src = i.img_url;
 
-  const cardHeadingWrapper = document.createElement("div");
-  cardHeadingWrapper.classList.add("card-heading-wrapper");
+    const cardHeadingWrapper = document.createElement("div");
+    cardHeadingWrapper.classList.add("card-heading-wrapper");
 
-  const title = document.createElement("h3");
-  title.innerText = i.title;
+    const title = document.createElement("h3");
+    title.innerText = i.title;
 
-  const price = document.createElement("h4");
-  price.classList.add("price");
-  const priceValue = parseFloat(i.price);
-  price.innerText = `${priceValue.toFixed(2)} €`;
+    const price = document.createElement("h4");
+    price.classList.add("price");
+    const priceValue = parseFloat(i.price);
+    price.innerText = `${priceValue.toFixed(2)} €`;
 
-  cardHeadingWrapper.append(title, price);
-  card.append(image, cardHeadingWrapper);
-  itemsWrapper.append(card);
-});
+    cardHeadingWrapper.append(title, price);
+    card.append(image, cardHeadingWrapper);
+    itemsWrapper.append(card);
+  });
 
 console.log(items);
